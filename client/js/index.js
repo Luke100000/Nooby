cade286aa8a8f75e6b2af7b695b40659fa7e8284
Luke100000 <jonas.peche@aon.at>
2020-05-29 11:05:36 +0200

    fixed all warnings and refactored

--- client/js/index.js
+++ client/js/index.js
@@ -1,20 +1,20 @@
-var noobyClient = new nooby;
+let noobyClient = new nooby;
 
-var wrapper = {
-    onmessage: function(msg){
+let wrapper = {
+    onmessage: function (msg) {
         console.log("[nooby] data received", msg)
     }
 }
 
-window.onclick = function(){
+window.onclick = function () {
     console.log(noobyClient.connection.readyState)
-    var start = new Date().getTime()
-    for(i=0; i<1;i++)
-        noobyClient.send({data:"Hello"})
+    let start = new Date().getTime()
+    for (let i = 0; i < 1; i++)
+        noobyClient.send({data: "Hello"})
     console.log("finished", new Date().getTime() - start)
-    
+
 }
 
-window.onload = function(){
+window.onload = function () {
     noobyClient.init(wrapper, "localhost", "25002");
 }
